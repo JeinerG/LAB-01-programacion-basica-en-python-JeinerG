63e34729bd6e336bd703f24fabf832fefc5a8720
JeinerG <jejgarciamo@unal.edu.co>
2026-05-14 09:14:45 -0500

    Lab_01..

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -12,7 +12,7 @@ def pregunta_12():
     la suma de los valores de la columna 5 sobre todo el archivo.
 
     Rta/
-    {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
+    {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}...
 
     """
     sums_by_letter = {}
